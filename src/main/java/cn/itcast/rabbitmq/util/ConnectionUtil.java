525838acdd4ea80c179ac7a7f0e7ca7483a87ed5
wh <1281947642@qq.com>
2020-01-01 13:23:04 +0800

    rabbitmq

--- src/main/java/cn/itcast/rabbitmq/util/ConnectionUtil.java
+++ src/main/java/cn/itcast/rabbitmq/util/ConnectionUtil.java
@@ -4,6 +4,7 @@ import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 
 public class ConnectionUtil {
+
     /**
      * 建立与RabbitMQ的连接
      * @return
@@ -13,13 +14,13 @@ public class ConnectionUtil {
         //定义连接工厂
         ConnectionFactory factory = new ConnectionFactory();
         //设置服务地址
-        factory.setHost("192.168.56.101");
+        factory.setHost("192.168.118.128");
         //端口
         factory.setPort(5672);
         //设置账号信息，用户名、密码、vhost
         factory.setVirtualHost("/leyou");
         factory.setUsername("leyou");
-        factory.setPassword("leyou");
+        factory.setPassword("123456");
         // 通过工程获取连接
         Connection connection = factory.newConnection();
         return connection;
