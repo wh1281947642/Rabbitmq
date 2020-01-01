220c67d37f064ec0f1f192efdc73e2e16364c327
wh <1281947642@qq.com>
2020-01-01 14:32:14 +0800

    rabbitmq

--- src/main/java/cn/itcast/rabbitmq/topic/Send.java
+++ src/main/java/cn/itcast/rabbitmq/topic/Send.java
@@ -4,6 +4,8 @@ import cn.itcast.rabbitmq.util.ConnectionUtil;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.MessageProperties;
+
 /**
  * 生产者，模拟为商品服务
  */
@@ -20,7 +22,7 @@ public class Send {
         // 消息内容
         String message = "新增商品 : id = 1001";
         // 发送消息，并且指定routing key 为：insert ,代表新增商品
-        channel.basicPublish(EXCHANGE_NAME, "item.insert", null, message.getBytes());
+        channel.basicPublish(EXCHANGE_NAME, "item.insert", MessageProperties.PERSISTENT_TEXT_PLAIN, message.getBytes());
         System.out.println(" [商品服务：] Sent '" + message + "'");
 
         channel.close();
